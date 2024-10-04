266c672ae4cf5e9eb2a91063992c1ccd2e9f3c8a
FelipeBarros10 <barrosfelipe735@gmail.com>
2024-10-04 17:43:13 -0300

    Até a aula 20

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,10 +19,10 @@
             <div class="card col-md-3">
                 <img src="/img/events/{{ $event->image }}" alt="{{ $event->title }}">
                 <div class="card-body">
-                    <p class="card-date">{{ date('d/m/y', strtotime($event->date)) }}</p>//transformando a data para a versão do Brasil
+                    <p class="card-date">{{ date('d/m/y', strtotime($event->date)) }}</p> <!--transformando a data para a versão do Brasil-->
                     <h5 class="card-title">{{ $event->title }}</h5>
                     <p class="card-participants">X Participantes</p>
-                    <a href="/events/{{ $event->id }}" class="btn btn-primary">Saber mais</a>//Link que vai direcionar para a rota '/events/{id}'
+                    <a href="/events/{{ $event->id }}" class="btn btn-primary">Saber mais</a> <!--Link que vai direcionar para a rota '/events/{id}'-->
                 </div>
             </div>
         @endforeach
